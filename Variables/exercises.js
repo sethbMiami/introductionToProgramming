35e203a4c3ebd7d4e56263d7659cf649b85b2232
Seth Bruner <bruner.seth@outlook.com>
2023-11-05 10:07:27 -0500

    added exercise files for section 2 and a directory

---
+++ Variables/exercises.js
@@ -0,0 +1,29 @@
+// {
+//   let foo = 'bar';
+// }
+
+// console.log(foo);
+
+// const NAME = 'Victor';
+// console.log('Good Morning, ' + NAME);
+// console.log('Good Afternoon, ' + NAME);
+// console.log('Good Evening, ' + NAME);
+
+// NAME = 'Joe';
+// console.log('Good Morning, ' + NAME);
+// console.log('Good Afternoon, ' + NAME);
+// console.log('Good Evening, ' + NAME);
+
+// let foo = 'bar';
+// {
+//   let foo = 'qux';
+// }
+
+// console.log(foo);
+
+const FOO = 'bar';
+{
+  const FOO = 'qux';
+}
+
+console.log(FOO);
